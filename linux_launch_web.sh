8727afbb8c854a2645b3f1b3c413a6319d47891c
JorgeEF <jorgefemenia@gmail.com>
2025-01-23 10:30:46 -0300

    fix terminal script launch

--- linux_launch_web.sh
+++ linux_launch_web.sh
@@ -3,7 +3,7 @@
 # Ejecutar el backend
 echo "Iniciando el backend..."
 cd backend || { echo "Error: no se pudo acceder al directorio 'backend'."; exit 1; }
-gnome-terminal -- bash -c "npm run start; exec bash" &
+xterm -e "npm run start" &
 
 # Volver al directorio raíz
 cd ..
@@ -11,6 +11,6 @@ cd ..
 # Servir el frontend desde la carpeta build en el puerto 80
 echo "Sirviendo el frontend en el puerto 80..."
 cd frontend || { echo "Error: no se pudo acceder al directorio 'frontend'."; exit 1; }
-gnome-terminal -- bash -c "npx serve -s dist -l 80; exec bash" &
+xterm -e "npx serve -s dist -l 80" &
 
 echo "Ambos servidores han sido iniciados."
