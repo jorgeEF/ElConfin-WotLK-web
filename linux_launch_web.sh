01ffe45c8d02d87737ca7071f48f7bcc82d8fa87
JorgeEF <jorgefemenia@gmail.com>
2025-01-23 10:40:37 -0300

    iniciar web en sesiones tmux

--- linux_launch_web.sh
+++ linux_launch_web.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Ejecutar el backend
-echo "Iniciando el backend..."
-cd backend || { echo "Error: no se pudo acceder al directorio 'backend'."; exit 1; }
-xterm -e "npm run start" &
+# Crear una nueva sesión para el backend
+tmux new-session -d -s web_backend "cd backend; npm run start"
 
-# Volver al directorio raíz
-cd ..
-
-# Servir el frontend desde la carpeta build en el puerto 80
-echo "Sirviendo el frontend en el puerto 80..."
-cd frontend || { echo "Error: no se pudo acceder al directorio 'frontend'."; exit 1; }
-xterm -e "npx serve -s dist -l 80" &
-
-echo "Ambos servidores han sido iniciados."
+# Crear una nueva sesión para el frontend
+tmux new-session -d -s web_frontend "cd frontend; npx serve -s dist -l 80"
