6fa6fe7e55173c8bab839f99f39318e8ec2e5bde
JorgeEF <jorgefemenia@gmail.com>
2025-02-01 12:07:59 -0300

    quitado InicioWow.css

--- frontend/src/pages/wow/ComoJugar.jsx
+++ frontend/src/pages/wow/ComoJugar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './InicioWow.css';
 import { Submenu } from '../../components/wow/Submenu'
 
 export const ComoJugar = () => {
